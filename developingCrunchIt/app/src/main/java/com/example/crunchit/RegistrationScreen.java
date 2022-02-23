00bb665595b5fcf1d571449204f267c5c62bd8ec
MrKeyur-CSE <keyur.developer.cse@gmail.com>
2022-02-22 20:39:16 -0500

    Firebase Connection

--- developingCrunchIt/app/src/main/java/com/example/crunchit/RegistrationScreen.java
+++ developingCrunchIt/app/src/main/java/com/example/crunchit/RegistrationScreen.java
@@ -2,14 +2,22 @@ package com.example.crunchit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_screen);
+
     }
 }
