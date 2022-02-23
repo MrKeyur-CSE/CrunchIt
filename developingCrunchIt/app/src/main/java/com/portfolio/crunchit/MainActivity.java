05483ac64f40c5cf8908517b0e3a2475b882581e
Daryl Valairan <darylvalairan@outlook.com>
2022-02-23 00:24:51 -0600

    Added some content to main activity

--- developingCrunchIt/app/src/main/java/com/portfolio/crunchit/MainActivity.java
+++ developingCrunchIt/app/src/main/java/com/portfolio/crunchit/MainActivity.java
@@ -1,14 +1,53 @@
 package com.portfolio.crunchit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    FirebaseUser currentUser;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+
+
+        TextView uName = findViewById(R.id.userName);
+        TextView email = findViewById(R.id.email);
+        TextView phone = findViewById(R.id.phNumber);
+
+        currentUser = auth.getCurrentUser();
+
+        myRef.child("Users").child(currentUser.getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if(task.isSuccessful()){
+                    User temp = task.getResult().getValue(User.class);
+                    uName.setText("Name  : " + temp.fullName);
+                    email.setText("Mail  : " +temp.email);
+                    phone.setText("Phone : " +temp.number);
+                }
+            }
+        });
+
     }
 }
