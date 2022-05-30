0318946a2a201c5c2d2b44c00b2239dce6e24268
Daryl Valairan <darylvalairan@outlook.com>
2022-05-30 09:08:15 -0400

    Minor changes

--- developingCrunchIt/app/src/main/java/com/portfolio/crunchit/Activities/moreDetailsActivity.java
+++ developingCrunchIt/app/src/main/java/com/portfolio/crunchit/Activities/moreDetailsActivity.java
@@ -1,15 +1,18 @@
 package com.portfolio.crunchit.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.portfolio.crunchit.R;
@@ -25,16 +28,50 @@ public class moreDetailsActivity extends AppCompatActivity {
     StorageReference storageReference;
     StorageReference thumbs;
 
-    TextView itemName;
+    TextView itemNameTV;
+    TextView itemPriceTV;
+    TextView itemDescriptionTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_details);
 
-        itemName = findViewById(R.id.itemName);
-
         Intent intent = getIntent();
 
-        itemName.setText(intent.getStringExtra("CurrentItem").toString());
+        itemNameTV = findViewById(R.id.itemName);
+        itemPriceTV = findViewById(R.id.itemPrice);
+        itemDescriptionTV = findViewById(R.id.itemDescription);
+
+
+        String itemId = intent.getStringExtra("CurrentItem").toString();
+        
+        database = FirebaseDatabase.getInstance();
+        
+        databaseRefRoot = database.getReference("Items/"+itemId);
+        databaseRefInventory = databaseRefRoot;
+
+        databaseRefInventory.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String itemId = snapshot.getKey().toString();
+                Log.e("------------------->", itemId);
+                String itemName = snapshot.child("itemName").getValue().toString();
+                String itemCost = snapshot.child("itemCost").getValue().toString();
+                String itemDescription = snapshot.child("itemDescription").getValue() != null?snapshot.child("itemDescription").getValue().toString():"No description found";
+
+                itemNameTV.setText(itemName);
+                itemPriceTV.setText(itemCost + " CAD");
+                itemDescriptionTV.setText(itemDescription);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
     }
 }
