29529fbb84ce97aea36b6d0054a2d61fe756932f
jasmineHere <jasminemuman21@gmail.com>
2022-06-13 17:42:27 -0400

    added layout

---
+++ developingOrderMgmt/app/src/main/java/com/portfolio/pickup/cardFragment.java
@@ -0,0 +1,14 @@
+package com.portfolio.pickup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cardFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_fragment);
+    }
+}
