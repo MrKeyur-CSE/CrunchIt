a3c2c9a5ca328930d0307599bb31f245ac41a8e4
MrKeyur-CSE <keyur.developer.cse@gmail.com>
2022-06-16 11:38:41 -0400

    Kevin's Changes

---
+++ developingCrunchIt/app/src/main/java/com/portfolio/crunchit/Activities/PaymentMethod.java
@@ -0,0 +1,16 @@
+package com.portfolio.crunchit.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.portfolio.crunchit.R;
+
+public class PaymentMethod extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_method);
+    }
+}
---
+++ developingCrunchIt/app/src/main/java/com/portfolio/crunchit/Activities/addnNewPayment.java
@@ -0,0 +1,16 @@
+package com.portfolio.crunchit.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.portfolio.crunchit.R;
+
+public class addnNewPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addn_new_payment);
+    }
+}
