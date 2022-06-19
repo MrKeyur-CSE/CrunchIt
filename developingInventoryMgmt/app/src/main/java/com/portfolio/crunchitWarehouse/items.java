0b932773473a35a9b2f2cb35373179f5ab44e642
MrKeyur-CSE <keyur.developer.cse@gmail.com>
2022-06-19 12:57:42 -0400

    items xml

--- developingInventoryMgmt/app/src/main/java/com/portfolio/crunchitWarehouse/items.java
+++ developingInventoryMgmt/app/src/main/java/com/portfolio/crunchitWarehouse/items.java
@@ -16,9 +16,9 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class Catagory extends AppCompatActivity {
+public class items extends AppCompatActivity {
 
-    FloatingActionButton addCatagory;
+    FloatingActionButton addItem;
     AlertDialog dialog;
     LinearLayout layout;
     ImageView backBtn;
@@ -26,18 +26,18 @@ public class Catagory extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_catagory);
+        setContentView(R.layout.activity_items);
 
-        backBtn = (ImageView) findViewById(R.id.backBtn);
-        addCatagory = findViewById(R.id.addCatagory);
-        layout = findViewById(R.id.cardCatagory);
-        
+        backBtn = findViewById(R.id.backAtItem);
+        addItem = findViewById(R.id.addItems);
+        layout = findViewById(R.id.cardItems);
+
         buildDialog();
 
-        addCatagory.setOnClickListener(new View.OnClickListener() {
+        addItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(Catagory.this, "add Button Clicked.", Toast.LENGTH_LONG).show();
+                Toast.makeText(items.this, "Clicked", Toast.LENGTH_LONG);
                 dialog.show();
             }
         });
@@ -45,40 +45,39 @@ public class Catagory extends AppCompatActivity {
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Catagory.this, MainActivity.class));
+                startActivity(new Intent(items.this, MainActivity.class));
             }
         });
     }
 
     private void buildDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        View view = getLayoutInflater().inflate(R.layout.activity_addcatagory, null);
+        View view = getLayoutInflater().inflate(R.layout.additem, null);
 
-        EditText name = view.findViewById(R.id.nameCatagory);
+        EditText name = view.findViewById(R.id.nameOfItem);
 
         builder.setView(view);
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        addCard(name.getText().toString());
-                    }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                addCard(name.getText().toString());
+            }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
 
-                    }
-                });
+            }
+        });
         dialog = builder.create();
     }
 
     private void addCard(String name) {
-        View view = getLayoutInflater().inflate(R.layout.activity_catgorycard, null);
-        TextView catagoryName = view.findViewById(R.id.catagoryName);
-        Button deletebtn = view.findViewById(R.id.deleteBtn);
+        View view = getLayoutInflater().inflate(R.layout.activity_itemcard, null);
+        TextView itemName = view.findViewById(R.id.itemName);
+        Button deleteBtn = view.findViewById(R.id.deleteItemBtn);
 
-        catagoryName.setText(name);
-        deletebtn.setOnClickListener(new View.OnClickListener() {
+        itemName.setText(name);
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 layout.removeView(view);
