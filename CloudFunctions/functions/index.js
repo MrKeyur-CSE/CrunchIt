6da3d53e8f2b1219f25707a8b75688b0ea6e92ff
Daryl Valairan <darylvalairan@outlook.com>
2022-07-18 09:17:48 -0400

    Push notifications work

--- CloudFunctions/functions/index.js
+++ CloudFunctions/functions/index.js
@@ -3,32 +3,27 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.sendListenerPushNotification = functions.database.ref("Orders").onWrite((data, context) => {
-  console.log(data);
-  console.log("<-------->");
-  console.log(context.params);
-});
 
-
-const FCMToken = admin.database().ref(`/Users/${getUserWhoPlacedOrder}/FCMToken/`).once("value");
-
-const payload = {
-  token: FCMToken,
-  notification: {
-    title: "cloud function demo",
-    body: "message",
-  },
-  data: {
-    body: "message",
-  },
-};
-
-admin.messaging().send(payload).then((response) => {
-  // Response is a message ID string.
-  console.log("Successfully sent message: ", response);
-  return {success: true};
-}).catch((error) => {
-  return {error: error.code};
-});
-
+exports.sendNotificationOnOrderUpdate = functions.database.ref('/Orders').onUpdate((snapshot, context) => {
+      const original = snapshot.before.val();
+      const changed = snapshot.after.val();
+      const payload = {
+            token: "f7P3QnO6Tead0wJfvNGzuW:APA91bG5xKEcEHUsBvNLZOYNTt0eLKANg8hFRu5LIqF_7L5TwpXLKXQ6mbg8rhnQNIErQbTtgT20uVd8potVd8mZdayOeQZGNFjVLD2oaP9xw91Tu1UwOLad-TxAlE0-xA9WBc53eGM5",
+          notification: {
+              title: 'cloud function demo',
+              body: "Hello people"
+          },
+          data: {
+              body: "This is the data body",
+          }
+      };
+      
+      admin.messaging().send(payload).then((response) => {
+          // Response is a message ID string.
+          console.log('Successfully sent message:', response);
+          return {success: true};
+      }).catch((error) => {
+          return {error: error.code};
+      });
+})
 
